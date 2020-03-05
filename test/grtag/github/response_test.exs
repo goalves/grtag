a58184cd473aa414f9f8041631eddd470c231a56
Gabriel Alves <g04lves@gmail.com>
2020-03-05 14:54:33 -0300

    [#development] Implement extra missing test for ensuring links are parsed

--- test/grtag/github/response_test.exs
+++ test/grtag/github/response_test.exs
@@ -1,8 +1,9 @@
 defmodule GRTag.Github.ResponseTest do
   use GRTag.DataCase, async: true
-  alias Tesla.Env
 
-  alias GRTag.Github.Response
+  alias Ecto.UUID
+  alias Tesla.Env
+  alias GRTag.Github.{Link, Response}
 
   @default_headers [{"key", "value"}]
 
@@ -20,6 +21,15 @@ defmodule GRTag.Github.ResponseTest do
     end
 
     test "returns a response structure containing links from the headers" do
+      user_id = UUID.generate()
+      next_url = "https://api.github.com/user/#{user_id}/starred?page=2"
+      last_url = "https://api.github.com/user/#{user_id}/starred?page=4"
+      headers_with_link = [{"link", "<#{next_url}>; rel=\"next\", <#{last_url}>; rel=\"last\""}]
+
+      env = env_fixture(headers_with_link)
+      assert %Response{links: link = %Link{}} = Response.parse(env, &decoding_function_mock/1)
+      assert link.last == last_url
+      assert link.next == next_url
     end
   end
 
